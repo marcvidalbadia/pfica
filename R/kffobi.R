e37968aa33550a98be2a0619a087c4ba5fde3acf
m-vidal <54290040+m-vidal@users.noreply.github.com>
2021-04-08 23:43:03 +0200

    Update kffobi.R

--- R/kffobi.R
+++ R/kffobi.R
@@ -6,70 +6,74 @@ kffobi <- function(fdx, ncomp = fdx$basis$nbasis, eigenfPar = fdPar(fdx),
   if (length(pr) != 1 & is.character(pr)) {
     pr <- "KL.st"
   } else if (!is.character(pr)) {
-    stop("Select a functional data object to project")
+    stop("Select a functional data object to project.")
   }
   if (center) {
     fdx <- center.fd(fdx)}
   a <- fdx$coefs
+  nrep <- ncol(a)
+  if (nrep < 2) 
+    stop("ICA not possible without replications.")
   phi <- fdx$basis
   rphi <- eigenfPar$fd$basis
   Lfdobj <- eigenfPar$Lfd
   lambda <- eigenfPar$lambda
-  L <- eval.penalty(rphi, 0)
+  J <- eval.penalty(rphi, 0)
   if (lambda > 0) {
     R <- eval.penalty(rphi, Lfdobj)
-    L <- L + lambda * R
-  }; L <- (L + t(L))/2
-  W <- solve(chol(L))
+    Gl <- J + lambda * R
+  }; Gl <- (Gl + t(Gl))/2
+  L <- chol(Gl)
+  Li <- solve(L)
   if (shrinkage == TRUE) {
-    cov <- corpcor::cov.shrink(t(a), verbose = F)
+    cov <- corpcor::cov.shrink(t(a), verbose = F)/nrep
   } else {
-    cov <- tcrossprod(a)/ncol(a) }
-  J <- inprod(rphi, phi)
-  rGram <- crossprod(W, J)
+    cov <- tcrossprod(a)/nrep }
+  G <- inprod(rphi, phi)
+  rGram <- crossprod(Li, G)
   C2 <- rGram %*% cov %*% t(rGram); C2  <- (C2 + t(C2))/2
   svdc <- La.svd(C2)
+  eigenc <- svdc$d
   u <- as.matrix(svdc$u[, 1:ncomp])
-  varprop <- svdc$d[1:ncomp]/sum(svdc$d)
-  b <- W %*% u
-  f <- fd(b, rphi)
-  z <- inprod(fdx, f)
-  svdz <- La.svd(crossprod(z))
-  ds <- diag(c(1/sqrt(svdz$d)))
-  wz <- ds %*% t(svdz$u)
+  Q <- solve(Gl) %*% G
+  Qs <- expm::sqrtm(Q)
+  b <-  Qs %*% solve(crossprod(Li,G)) %*% u
+  #diag(t(b)%*%J%*%b) #check norms
+  gamma <- fd(b,bbasis)
+  z <- inprod(fdx, gamma)
+  #plot(eval.fd(arg,fd(b%*%t(z),bbasis)[20]), col="red", type="l")
+  svdz <- La.svd(crossprod(z)/nrep)
+  wz <- svdz$u %*% diag(c(1/sqrt(svdz$d)))%*% t(svdz$u)
   zst <- z %*% wz
   nr <- sqrt(rowSums(zst^2))
   z.st <- nr * zst
-  C4 <- crossprod(z.st)/(ncol(a) * (ncomp + 2))
+  C4 <- crossprod(z.st)/(nrep * (ncomp + 2))
   svdk <- La.svd(C4)
   eigenk  <- svdk$d
   v <- svdk$u
   c <- b %*% v
-  h <- fd(c, rphi)
-  V <- La.svd(cov)
+  #diag(t(c)%*%J%*%c) #check norms
+  psi <- fd(c, phi)
+  Ls <- chol(G)
+  V2 <- Ls %*% cov %*% t(Ls); C2  <- (C2 + t(C2))/2
+  V <- La.svd(V2)
   wa <- V$u %*% diag(c(1/sqrt(V$d))) %*% t(V$u)
-  wa <- sqrt(ncol(a)-1)*wa
-  ast <- wa %*% a
-  xst <- fd(ast,phi)
+  ast <- solve(Ls) %*% wa %*% Ls %*% a
+  xst <- fd(ast, phi)
   KL <- fd(b %*% t(z), phi)
   KLst <- fd(b %*% t(zst), phi)
   project <- list(fdx, xst, KL, KLst)
   names(project) <- c("fdx", "fdx.st", "KL", "KL.st")
-  zi <- inprod(project[[paste(pr)]], h)
-  kurt <- vector()
-  for (i in 1:ncomp) {
-    K <- kurtosis(zi[,i])
-    kurt[i]=K}
+  zi <- inprod(project[[paste(pr)]], psi)
   if (plotfd) {
-    oldpar <- par(mfrow=c(2,3), no.readonly = TRUE)
+    oldpar <- par(mfrow=c(1,2), no.readonly = TRUE)
     on.exit(par(oldpar))
     for (j in 1:ncomp){
-      plot(h[j])
-      title(paste('IC', j, "Kurt:", round(kurt[j],3)))
+      plot(psi[j])
+      title(paste('IC', j, "Kurt:", round(eigenk[j],3)))
       par(ask=T)}; par(ask=F)}
-  colnames(h$coefs) <- paste("eigenf.", c(1:ncomp), sep = "-")
-  rownames(h$coefs) <- h$basis$names
-  FICA <- list(h, kurt, zi)
+  colnames(psi$coefs) <- paste("eigenf.", c(1:ncomp), sep = "-")
+  rownames(psi$coefs) <- psi$basis$names
+  FICA <- list(psi, eigenk, zi)
   names(FICA) <- c("eigenbasis", "kurtosis", "scores")
-  return(FICA)
 }

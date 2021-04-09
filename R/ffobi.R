07501837e7c7c62d44e09c23a5bd53e8c06b2d4f
m-vidal <54290040+m-vidal@users.noreply.github.com>
2021-04-09 10:12:13 +0200

    Update ffobi.R

--- R/ffobi.R
+++ R/ffobi.R
@@ -17,7 +17,6 @@ ffobi <- function(fdx, ncomp = fdx$basis$nbasis, eigenfPar = fdPar(fdx),
   if (center) 
     fdx <- center.fd(fdx);
   
-  nrep <- dim(a)[2]
   phi <- fdx$basis
   G <- inprod(phi, phi)
   L1 <- chol(G)
@@ -51,13 +50,12 @@ ffobi <- function(fdx, ncomp = fdx$basis$nbasis, eigenfPar = fdPar(fdx),
   C4 <- rGram %*% kurt %*% t(rGram)
   C4  <- (C4 + t(C4))/2
   
-  #Alternative discretized version of the Kurtosis operator
   #C4 <- rGram%*%asta%*%t(asta)%*%t(rGram)%*%rGram%*%asta%*%t(asta)%*%t(rGram)
   #C4  <- (C4 + t(C4))/2
   
   svdk <- La.svd(C4)
   u <- as.matrix(svdk$u[, 1:ncomp])
-  eigv <- svdk$d[1:ncomp]
+  eigenk <- svdk$d[1:ncomp]/sum(svdk$d)
   b <- W %*% u
   h <- fd(b, rphi)
   xst <- fd(asta, phi)
@@ -65,21 +63,17 @@ ffobi <- function(fdx, ncomp = fdx$basis$nbasis, eigenfPar = fdPar(fdx),
   names(project) <- c("fdx", "fdx.st")
   zi <- inprod(project[[paste(pr)]], h)
   
-  kurt <- vector()
-  for (i in 1:ncomp) {
-    K <- moments::kurtosis(zi[,i])
-    kurt[i]=K}
-  
   if (plotfd) {
-    oldpar <- par(mfrow=c(2,3), no.readonly = TRUE)
+    oldpar <- par(mfrow=c(1,2), no.readonly = TRUE)
     on.exit(par(oldpar))
     for (j in 1:ncomp){
       plot(h[j])
-      title(paste('IC', j, "Kurt:", round(kurt[j],3)))
+      title(paste('IC', j, "Kurt:", round(eigenk[j],3)))
       par(ask=T)}; par(ask=F)}
   colnames(h$coefs) <- paste("eigenf.", c(1:ncomp), sep = "-")
   rownames(h$coefs) <- h$basis$names
-  FICA <- list(h, eigv, kurt, zi)
-  names(FICA) <- c("eigenbasis", "eigenvalues", "kurtosis", "scores")
+  FICA <- list(h, eigenk, zi)
+  names(FICA) <- c("eigenbasis", "kurtosis", "scores")
   return(FICA)
 }
+

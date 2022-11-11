71d79ae97a9bebfa41ba610f91968b750b27a4d9
Marc Vidal <54290040+m-vidal@users.noreply.github.com>
2022-11-11 10:48:18 +0100

    Update pspline.kffobi.R

--- R/pspline.kffobi.R
+++ R/pspline.kffobi.R
@@ -1,24 +1,23 @@
 pspline.kffobi <- function(fdx, ncomp = fdx$basis$nbasis, pp = 0, r = 2,
-                           pr = c("fdx", "fdx.st", "KL", "KL.st"),
-                           shrinkage = FALSE, center = FALSE, plotfd = FALSE) {
+                           pr = c("fdx", "wfdx", "KL", "wKL"),
+                           center = FALSE) {
   if (!(inherits(fdx, "fd")))
     stop("Argument FD  not a functional data object; see fda package.")
   if (length(pr) != 1 & is.character(pr)) {
-    pr <- "KL.st"
+    pr <- "wKL"
   } else if (!is.character(pr)) {
     stop("Select a functional data object to project.")
   }
-  if (center) {
-    fdx <- center.fd(fdx)
-  }
+  
+  if (center) fdx <- center.fd(fdx)
+
   a <- fdx$coefs
   nrep <- ncol(a)
   if (nrep < 2)
     stop("ICA not possible without replications.")
+
   phi <- fdx$basis
-  if (shrinkage == TRUE) {
-    cov <- corpcor::cov.shrink(t(a), verbose = F)/nrep
-  } else { cov <- tcrossprod(a)/nrep }
+  cov <- tcrossprod(a)/nrep
   delta <- diff(diag(nrow(a)), differences = r)
   Pr <- crossprod(delta)
   G <- inprod(phi, phi)
@@ -26,18 +25,32 @@ pspline.kffobi <- function(fdx, ncomp = fdx$basis$nbasis, pp = 0, r = 2,
   L <- chol(Gl)
   Li <- solve(L)
   rGram <- crossprod(Li,G)
-  C2 <- rGram %*% cov %*% t(rGram); C2  <- (C2 + t(C2))/2
+  C2 <- rGram %*% cov %*% t(rGram)
+  C2  <- (C2 + t(C2))/2
   svdc <- La.svd(C2)
   u <- as.matrix(svdc$u[, 1:ncomp])
+
   Q <- solve(Gl) %*% G
   Qs <- expm::sqrtm(Q)
   b <-  Qs %*% solve(t(Li) %*% G) %*% u
-  #diag(t(b)%*%G%*%b) #check norms
-  beta <- fd(b,phi)
-  z <- inprod(fdx, beta)
+  #print(diag(t(b)%*%G%*%b)) #check norms
+
+  b3 <-  Li %*% u
+  f3 <- fd(b3,phi);
+  G2 <- inprod(f3,f3)
+  Gram2 <- chol(G2)
+  Li3  <- solve(Gram2)
+  b2 <- b3%*%Li3
+  beta <- fd(b2,phi)
+  #print(inprod(beta,beta)) #check orthonormality
+
+  z <-  t(t(u)%*%rGram%*%a)
+  #print(crossprod(z)/nrep) #check uncorrelatedness
+
   svdz <- La.svd(crossprod(z)/nrep)
   wz <- svdz$u %*% diag(c(1/sqrt(svdz$d)))%*% t(svdz$u)
   zst <- z %*% wz
+  #crossprod(zst)/64
   nr <- sqrt(rowSums(zst^2))
   z.st <- nr * zst
   C4 <- crossprod(z.st)/(nrep * (ncomp + 2))
@@ -47,39 +60,36 @@ pspline.kffobi <- function(fdx, ncomp = fdx$basis$nbasis, pp = 0, r = 2,
   c <- b %*% v
   #diag(t(c)%*%G%*%c) #check norms
   psi <- fd(c, phi)
+  #print(inprod(psi,psi)) #check orthonormality
   Ls <- chol(G)
   V2 <- Ls %*% cov %*% t(Ls)
   V2  <- (V2 + t(V2))/2
   V <- La.svd(V2)
   wa <- V$u %*% diag(c(1/sqrt(V$d))) %*% t(V$u)
   ast <- solve(Ls) %*% wa %*% Ls %*% a
+  print(diag(Ls%*%crossprod(t(ast))%*%t(Ls))/nrep)
   xst <- fd(ast, phi)
+
   KL <- fd(b %*% t(z), phi)
   KLst <- fd(b %*% t(zst), phi)
-  project <- list(fdx, xst, KL, KLst)
-  names(project) <- c("fdx", "fdx.st", "KL", "KL.st")
-  zi <- inprod(project[[paste(pr)]], psi)
+  project <- list(fdx, KL)
+  names(project) <- c("fdx", "KL")
 
-  kurt <- vector()
-  for (i in 1:ncomp) kurt[i] <- moments::kurtosis(zi[,i])
+  if (pr ==  "wKL") {
+    zi <- zst %*% v
+  } else if (pr ==  "wfdx") {
+    zi <- t(v%*%t(b)%*%G%*%ast)
+  }  else {
+    zi <- inprod(project[[paste(pr)]], psi)
+  }
+  #print(crossprod(zi)/nrep) #check orthonormality
 
-  if (plotfd) {
-    oldpar <- par(mfrow=c(1,2), no.readonly = TRUE)
-    on.exit(par(oldpar))
-    for (j in 1:ncomp){
-      plot(psi[j])
-      title(paste('IC', j, "Kurt:", round(eigenk[j],3)))
-      par(ask=T)}; par(ask=F)}
-  colnames(psi$coefs) <- paste("eigenf.", c(1:ncomp), sep = "-")
-  rownames(psi$coefs) <- psi$basis$names
-
-  FICA <- list(svdc$d,beta,z,svdk$d,psi,zi,kurt)
+  FICA <- list(svdc$d,beta,z,svdk$d,psi,zi)
   names(FICA) <- c("PCA.eigv",
                    "PCA.basis",
                    "PCA.scores",
                    "ICA.eigv",
                    "ICA.basis",
-                   "ICA.scores",
-                   "ICA.kurtosis")
+                   "ICA.scores")
   return(FICA)
 }
